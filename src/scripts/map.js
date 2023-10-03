8f9a8bd3789e954b2f9de82eb60c49de67c2895f
danila85 <pogranichnik.marshal@yandex.ru>
2023-10-03 17:10:44 +0300

    Улучшение архитектуры методов класса Map.

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -16,29 +16,30 @@ export class Map {
         context.stroke();
     }
 
-    static createRow(context, length = 1, basePositionX = 0, basePositionY = 0, baseWidth = 10, baseHeight = 10, baseStrokeColor = "#000", positionY = 0, localPositionX = 0, localPositionY = 0) {
-        let positionX = basePositionX;
+    static createRow(context, length = 1, positionX = 0, positionY = 0, localPositionX = 0, localPositionY = 0, generalWidth = 10, generalHeight = 10, generalStrokeColor = "#000") {
         localPositionX = 0;
 
         for (let i = 0; i < length; i++) {
-            this.createCeil(context, positionX, positionY, baseWidth, baseHeight, baseStrokeColor);
+            this.createCeil(context, positionX, positionY, generalWidth, generalHeight, generalStrokeColor);
             this.positions.push(new PositionCeil());
             this.positions.at(-1).globalPositionX = positionX;
             this.positions.at(-1).globalPositionY = positionY;
             this.positions.at(-1).localPositionX = localPositionX;
             this.positions.at(-1).localPositionY = localPositionY;
             localPositionX++;
-            positionX += baseWidth;
+            positionX += generalWidth;
         }
     }
 
     static createRegion(context, rows = 1, length = 1, basePositionX = 0, basePositionY = 0, generalWidth = 10, generalHeight = 10, generalStrokeColor = "#000") {
+        let positionX = basePositionX;
         let positionY = basePositionY;
+
         let locaclPositionY = 0;
 
         for (let i = 0; i < rows; i++) {
             let localPositionX = 0;
-            this.createRow(context, length, basePositionX, positionY, generalWidth, generalHeight, generalStrokeColor, positionY, localPositionX, locaclPositionY);
+            this.createRow(context, length, positionX, positionY, localPositionX, locaclPositionY, generalWidth, generalHeight, generalStrokeColor);
             locaclPositionY++;
             positionY += generalHeight;
         }
